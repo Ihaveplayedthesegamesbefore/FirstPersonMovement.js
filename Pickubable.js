dee68da38b07f370f92bc827117c84a717fd538b
Ihaveplayedthesegamesbefore <daluaralu101@gmail.com>
2025-04-25 20:11:12 -0400

    Update Pickubable.js

--- Pickubable.js
+++ Pickubable.js
@@ -1,1 +1,14 @@
+var Pickupable = pc.createScript('pickupable');
 
+// Initialize code called once per entity
+Pickupable.prototype.initialize = function() {
+    // Ensure the entity has a rigidbody component
+    if (!this.entity.rigidbody) {
+        console.error("Pickupable script needs to have a 'rigidbody' component");
+    }
+};
+
+// Update code called every frame
+Pickupable.prototype.update = function(dt) {
+    // No update logic needed for this script
+};
